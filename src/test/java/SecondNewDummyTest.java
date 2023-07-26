2f824725c2725e549e8b691c943b6a21cac44d04
ollven <ollven@yandex.ru>
2023-07-26 15:30:41 +0200

    Add one more test class with parralel feature enabled

---
+++ src/test/java/SecondNewDummyTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SecondNewDummyTest {
+
+    @Test
+    public void testThatAlwaysFails() throws InterruptedException {
+        Thread.sleep(1000);
+        Assertions.fail();
+    }
+
+    @Test
+    public void testThatAlwaysPasses() throws InterruptedException {
+        Thread.sleep(1000);
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void testThatIsSlowButAlwaysFails() throws InterruptedException {
+        Thread.sleep(1000);
+        Assertions.fail();
+    }
+
+    @Test
+    public void testThatIsSlowButAlwaysPasses() throws InterruptedException {
+        Thread.sleep(1000);
+        Assertions.assertTrue(true);
+    }
+}
