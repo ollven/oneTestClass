ca6536e9f7519386f73b34ce8f745cb46ce9e86d
ollven <ollven@yandex.ru>
2023-08-11 14:34:55 +0200

    make one tests fail

--- src/test/java/DummyTest.java
+++ src/test/java/DummyTest.java
@@ -17,7 +17,7 @@ public class DummyTest {
     @Test
     public void testThatIsSlowButAlwaysFails() throws InterruptedException {
         Thread.sleep(1000);
-        Assertions.assertTrue(true);
+        Assertions.fail();
     }
 
     @Test
