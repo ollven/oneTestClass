9df1a7267072fd7488a6909de729b8dacb963fed
ollven <ollven@yandex.ru>
2024-01-08 17:04:53 +0100

    58

--- src/test/java/SecondDummyTest.java
+++ src/test/java/SecondDummyTest.java
@@ -23,7 +23,7 @@ public class SecondDummyTest {
 
     @Test
     public void testThatIsSlowButAlwaysPasses() throws InterruptedException {
-        Thread.sleep(3800);
+        Thread.sleep(3900);
         Assertions.assertTrue(true);
     }
 }
