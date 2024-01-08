e5fb33f00372a45901484434084ae2cc3752d841
ollven <ollven@yandex.ru>
2024-01-08 16:32:20 +0100

    14

--- src/test/java/NewDummyTest.java
+++ src/test/java/NewDummyTest.java
@@ -11,7 +11,7 @@ public class NewDummyTest {
 
     @Test
     public void testThatAlwaysPasses() throws InterruptedException {
-        Thread.sleep(1000);
+        Thread.sleep(1100);
         Assertions.assertTrue(true);
     }
 
