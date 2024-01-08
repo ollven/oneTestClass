3e73b7549513c28497a2986632179a9f362f16bf
Olga Sventukh <ollven@yandex.ru>
2024-01-08 16:05:14 +0100

    Update SecondNewDummyTest.java

--- src/test/java/SecondNewDummyTest.java
+++ src/test/java/SecondNewDummyTest.java
@@ -17,7 +17,7 @@ public class SecondNewDummyTest {
 
     @Test
     public void testThatIsSlowButAlwaysFails() throws InterruptedException {
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         Assertions.assertTrue(true);
     }
 
