fbbde8336ce6f123176bb7c4f1171e0b66d9b372
Olga Sventukh <olga.sventukh@jetbrains.com>
2024-10-01 12:49:28 +0000

    ggdstge

--- src/test/java/DummyTest.java
+++ src/test/java/DummyTest.java
@@ -17,7 +17,7 @@ public class DummyTest {
     @Test
     public void testThatIsSlowButAlwaysFails() throws InterruptedException {
         Thread.sleep(1000);
-        Assertions.assertTrue(false);
+        Assertions.assertTrue(true);
     }
 
     @Test
